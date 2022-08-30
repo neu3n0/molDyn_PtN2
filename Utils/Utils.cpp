3c619b53a3f8dca66119eea935ff70dc043d9c72
neu3n0 <neu3n0.sH@gmail.com>
2022-08-30 13:10:02 +0300

    fix gitignore & Utils

--- Utils/Utils.cpp
+++ Utils/Utils.cpp
@@ -1,4 +1,4 @@
-#include "Utils.h"
+#include "Utils.hpp"
 
 std::vector<std::string> Utils::parseLine3(std::string iniStr) {
     std::vector<std::string> result;
@@ -22,4 +22,45 @@ std::vector<std::string> Utils::parseLine3(std::string iniStr) {
         result.push_back(iniStr.substr(startPos, endPos - startPos));
     }
     return result;
+}
+
+bool Utils::renorm(std::vector<double>& s) {
+    double length(0);
+    for (size_t i = 0; i < 3; ++i) length += s[i] * s[i];
+    length = sqrt(length);
+    if (length < 1e-10) return false;
+    for (size_t i = 0; i < 3; ++i) s[i] /= length;
+    return true;
+}
+
+std::vector<double> Utils::vecProd(const std::vector<double>& A, const std::vector<double>& B) {
+    std::vector<double> res(3);
+    res[0] = A[1] * B[2] - A[2] * B[1];
+    res[1] = A[2] * B[0] - A[0] * B[2];
+    res[2] = A[0] * B[1] - A[1] * B[0];
+    return res;
+}
+
+double Utils::scalProd(const std::vector<double>& A, const std::vector<double>& B) {
+    double res(0);
+    res += A[0] * B[0];
+    res += A[1] * B[1];
+    res += A[2] * B[2];
+    return res;
+}
+
+std::vector<double> Utils::vecProd(double* A, double* B) {
+    std::vector<double> res(3);
+    res[0] = A[1] * B[2] - A[2] * B[1];
+    res[1] = A[2] * B[0] - A[0] * B[2];
+    res[2] = A[0] * B[1] - A[1] * B[0];
+    return res;
+}
+
+double Utils::scalProd(double* A, double* B) {
+    double res(0);
+    res += A[0] * B[0];
+    res += A[1] * B[1];
+    res += A[2] * B[2];
+    return res;
 }
--- Utils/Utils.hpp
+++ Utils/Utils.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <cmath>
 
 class Utils {
 public:
     static std::vector<std::string> parseLine3(std::string iniStr);
+    static bool renorm(std::vector<double>& s);
+    static std::vector<double> vecProd(const std::vector<double>& A, const std::vector<double>& B);
+    static double scalProd(const std::vector<double>& A, const std::vector<double>& B);
+    static std::vector<double> vecProd(double* A, double* B);
+    static double scalProd(double* A, double* B);
 };
