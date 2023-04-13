6acb47c537dd49ab522e9d52570ca513ec4651bd
neu3n0 <neu3n0.sH@gmail.com>
2023-04-13 15:08:53 +0300

    add script for start calcs

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright (c) 2022 Insilico Medicine
+
+if [ -z $1 ] 
+then
+    echo "add number of conformations"
+    exit 1
+fi
+
+if [ -z $2 ] 
+then
+    echo "add name of folder for save results"
+    exit 1
+fi
+
+nConf=$1
+folder_name=$2
+
+make prepare_mols_info && make md
+
+./prepare_mols_info --cfg input/config.txt --cfginp input/configInp.txt --confs $nConf --out confs.txt
+time ./md --cfg input/config.txt --inp input/initAtoms.inp --cfginp input/confs.txt > log
+
+cd "calcs/"
+mkdir $folder_name
+cd $folder_name
+mv ../../output/calcs/* .
+mv ../../input/confs.txt .
+mv ../../log .
+cd ../..
